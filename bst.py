a4b6d7f017d3ece070a2e2a954b2a23391ece07e
hezam <kafe.hezam@zendesk.com>
2021-09-16 17:15:32 -0700

    Add new method to insert node recursivly

--- bst.py
+++ bst.py
@@ -17,4 +17,17 @@ class BST:
             self.insert_node_rec(self.root, new_node)
         else:
             self.insert_node_iter(self.root, new_node)
+        return
+
+    def insert_node_rec(self, node, new_node):
+        if node.element < new_node.element:
+            if not node.right:
+                node.right = new_node 
+            else:
+                self.insert_node_rec(node.right, new_node)
+        else:
+            if node.left:
+                node.left = new_node 
+            else: 
+                self.insert_node_rec(node.left, new_node)
         return 
